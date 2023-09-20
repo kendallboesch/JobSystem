9c6f41af261e0a354f11de7a1427bde0c7e9028d
Kendall Boesch <kboesch@smu.edu>
2023-09-20 12:26:34 -0500

    Transferred from different repo

--- Code/main.cpp
+++ Code/main.cpp
@@ -8,82 +8,82 @@
 int main(void ) {
     std::cout << "Creating Job System" << std::endl;
 
-   //JobSystem* js = JobSystem::CreateOrGet(); 
+    JobSystem* js = JobSystem::CreateOrGet(); 
 
     std::cout << "Creating worker threads" << std::endl; 
 
-    // js->CreateWorkerThread("Thread1", 0xFFFFFFFF);
-    // js->CreateWorkerThread("Thread2", 0xFFFFFFFF);
-    // js->CreateWorkerThread("Thread3", 0xFFFFFFFF);
-    // js->CreateWorkerThread("Thread4", 0xFFFFFFFF);
-    // js->CreateWorkerThread("Thread5", 0xFFFFFFFF);
-    // js->CreateWorkerThread("Thread6", 0xFFFFFFFF);
-    // js->CreateWorkerThread("Thread7", 0xFFFFFFFF);
-    // js->CreateWorkerThread("Thread8", 0xFFFFFFFF);
-    // js->CreateWorkerThread("Thread9", 0xFFFFFFFF);
+    js->CreateWorkerThread("Thread1", 0xFFFFFFFF);
+    js->CreateWorkerThread("Thread2", 0xFFFFFFFF);
+    js->CreateWorkerThread("Thread3", 0xFFFFFFFF);
+    js->CreateWorkerThread("Thread4", 0xFFFFFFFF);
+    js->CreateWorkerThread("Thread5", 0xFFFFFFFF);
+    js->CreateWorkerThread("Thread6", 0xFFFFFFFF);
+    js->CreateWorkerThread("Thread7", 0xFFFFFFFF);
+    js->CreateWorkerThread("Thread8", 0xFFFFFFFF);
+    js->CreateWorkerThread("Thread9", 0xFFFFFFFF);
 
-    // std::cout << "Create jobs" << std::endl; 
+    std::cout << "Create jobs" << std::endl; 
 
-    // std::vector<Job*> jobs; 
+    std::vector<Job*> jobs; 
 
-    // CompileJob* cjb = new CompileJob(0xFFFFFFFF, 1); 
-    // jobs.push_back(cjb); 
-
-
-    // for(int j = 0; j < 10; j++)
-    // {
-    //     RenderJob* rjb = new RenderJob(0xFFFFFFFF, 1); 
-    //     for(int i = 0; i < 1000; i++)
-    //     {
-    //         rjb->data.push_back(i+j); 
-    //     }
-    //     jobs.push_back(rjb);
-    // }
+    CompileJob* cjb = new CompileJob(0xFFFFFFFF, 1); 
+    jobs.push_back(cjb); 
 
 
-    // std::cout << "Queueing Jobs" << std::endl; 
+    for(int j = 0; j < 10; j++)
+    {
+        RenderJob* rjb = new RenderJob(0xFFFFFFFF, 1); 
+        for(int i = 0; i < 1000; i++)
+        {
+            rjb->data.push_back(i+j); 
+        }
+        jobs.push_back(rjb);
+    }
 
-    // std::vector<Job*>::iterator it = jobs.begin(); 
 
-    // for(; it != jobs.end(); ++it)
-    // {
-    //     js->QueueJob(*it); 
-    // }
+    std::cout << "Queueing Jobs" << std::endl; 
 
-    // int running = 1; 
+    std::vector<Job*>::iterator it = jobs.begin(); 
 
-    // while(running)
-    // {
-    //     std::string command; 
-    //     std::cout << "Enter stop, destroy, finifh, statsu" << std::endl; 
-    //     std::cin >> command; 
+    for(; it != jobs.end(); ++it)
+    {
+        js->QueueJob(*it); 
+    }
 
-    //     if(command == "stop")
-    //     {
-    //         std::cout << "stopping" << std::endl; 
-    //         running = 0; 
-    //     }
-    //     else if(command == "destroy")
-    //     {
-    //         js->FinishCompletedJobs();
-    //         js->Destory(); 
-    //         running = 0; 
-    //     }
-    //     else if(command == "finish")
-    //     {
-    //         js->FinishJob(0); 
+    int running = 1; 
 
-    //     }
-    //     else if (command == "status")
-    //     {
-    //         std::cout << "Job status " << js->GetJobStatus(0) << std::endl; 
-    //     }
-    //     else 
-    //     {
-    //         std::cout <<"Invalid command" << std::endl; 
-    //     }
+    while(running)
+    {
+        std::string command; 
+        std::cout << "Enter stop, destroy, finifh, statsu" << std::endl; 
+        std::cin >> command; 
+
+        if(command == "stop")
+        {
+            std::cout << "stopping" << std::endl; 
+            running = 0; 
+        }
+        else if(command == "destroy")
+        {
+            js->FinishCompletedJobs();
+            js->Destory(); 
+            running = 0; 
+        }
+        else if(command == "finish")
+        {
+            js->FinishJob(0); 
+
+        }
+        else if (command == "status")
+        {
+            std::cout << "Job status " << js->GetJobStatus(0) << std::endl; 
+        }
+        else 
+        {
+            std::cout <<"Invalid command" << std::endl; 
+        }
         
-    // }
+    }
 
 
     return 0;
